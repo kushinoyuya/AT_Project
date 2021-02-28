78d963596b0189ec9ccc94b0e227b4689d589f6d
kushinoyuya <kusshy15059855@gmail.com>
2021-02-28 12:21:01 +0900

    comb_sort

--- script_practice/Test.py
+++ script_practice/Test.py
@@ -12,3 +12,50 @@
 #     bubble_sort(nums)
 #     print(bubble_sort(nums))
 ################## Coctail Sort
+# def coctail_sort(numbers):
+#     len_numbers = len(numbers)
+#     swapped = True
+#     start = 0
+#     end = len_numbers - 1
+#     while swapped:
+#         swapped = False
+#         for i in range(start,end):
+#             if numbers[i] > numbers[i+1]:
+#                 numbers[i], numbers[i+1] = numbers[i+1], numbers[i]
+#                 swapped = True
+#         if not swapped:
+#             break
+#         swapped = False
+#         end = end - 1
+#         for i in range(end-1, start-1, -1):
+#             if numbers[i] > numbers[i + 1]:
+#                 numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
+#                 swapped = True
+#         start = start + 1
+#     return numbers
+# if __name__ == "__main__":
+#     import random
+#     num = [random.randint(0,1000) for _ in range(10)]
+#     print(coctail_sort(num))
+################## Comb Sort
+def comb_sort(numbers):
+    len_numbers = len(numbers)
+    gap = len_numbers
+    swapped = True
+    while gap != 1:
+        gap = int(gap / 1.3)
+        if gap < 1:
+            gap - 1
+        swapped = False
+        for i in range(0, len_numbers - gap):
+            if numbers[i] > numbers[i + gap]:
+                numbers[i], numbers[i+gap]=numbers[i+gap],numbers[i]
+                swapped = True
+            pass
+    return numbers
+
+if __name__ == "__main__":
+    import random
+    nums = [random.randint(0,1000) for _ in range(10)]
+    comb_sort(nums)
+    print(comb_sort(nums))
